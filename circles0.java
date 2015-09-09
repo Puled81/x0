b985b1aedf8bcae1ad59244eff37e8f8a3fcd4a7
Puled81 <drakep@verizon.net>
2015-09-09 11:03:19 -0400

    Bigger

--- circles0.java
+++ circles0.java
@@ -2,10 +2,10 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(300, 300);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 80,90, 70,70 );       // Try changing the (x,y) and size.
 }
